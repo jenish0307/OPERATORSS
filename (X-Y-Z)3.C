de8cedd7ac6b29a8070a4f81df262eaf9bdae471
jenish0307 <131126665+jenish0307@users.noreply.github.com>
2023-08-05 10:19:42 +0530

    Update (X-Y-Z)3.C

--- (X-Y-Z)3.C
+++ (X-Y-Z)3.C
@@ -10,7 +10,7 @@ printf("enter the value of y : ");
 scanf("%d",&y);
 printf("enter the value of z : ");
 scanf("%d",&z);
-area=(x*x*x)-(y*y*y)-(z*z*z)-(3*(x+y)*(y+z)*(z+x));
+area=(x*x*x)-(y*y*y)-(z*z*z)-(3*(x+y))+(3*(y+z))+(3*(z+x));
 printf("x - y - z cube = %d",area);
 getch();
 }
